d429dce31119d8787a3d5bac86681c7b179b1ba9
Ruben <rubenwhite3005@gmail.com>
2024-07-24 20:06:38 +0100

    git push

--- dist/js/data/sets/gen9.js
+++ dist/js/data/sets/gen9.js
@@ -17,5 +17,40 @@ var SETDEX_SV = {
         "moves": ["Flame Charge"]
 
 }
+},
+
+    "Quaxly": {
+    "Rival May | Route 103": {  //if second pokemon
+        "index": "0000000002",
+        "level": 7,
+        "ability": "Moxie",
+        "item": "Oran Berry", 
+        "nature": "Adamant",
+        "ivs": {
+            "hp": 31,
+            "at": 31,
+            "df": 31,
+            "sa": 31,
+            "sd": 31,
+            "sp": 31
+        },
+        "moves": ["Aqua Jet"]
+
+}, "Youngster Larry | Route 102": {  //if second pokemon
+        "index": "0000000003",
+        "level": 7,
+        "ability": "Intimidate",
+        "item": "Oran Berry", 
+        "nature": "Adamant",
+        "ivs": {
+            "hp": 31,
+            "at": 31,
+            "df": 31,
+            "sa": 31,
+            "sd": 31,
+            "sp": 31
+        },
+        "moves": ["Flame Charge"]
+}
 }
 }
