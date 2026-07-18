12ee40ddb5fba51f56bd66adb3cb4752c5d544dc
Ruben White <rubenwhite3005@gmail.com>
2026-07-18 12:48:13 +0100

    W

--- js/data/sets/gen9.js
+++ js/data/sets/gen9.js
@@ -3360,7 +3360,7 @@ var SETDEX_SV = {
             "ivs": {
                 "hp": 31, "at": 31, "df": 31, "sa": 31, "sd": 31, "sp": 31
             },
-            "moves": ["Dragon Cheer","Icicle Spear","Hydro Pump","Megahorn"]
+            "moves": ["Dragon Dance","Icicle Spear","Hydro Pump","Megahorn"]
         }
     },
     "Dusknoir": {
